347771cea54ab3e9e5f96e9ee9bb93c97527ee74
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-21 09:37:20 +0200

    Initial Commit

---
+++ LearnAPI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearnAPI
+//
+//  Created by Saiman Chen on 2022-10-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnAPI/LearnAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAPIApp.swift
+//  LearnAPI
+//
+//  Created by Saiman Chen on 2022-10-21.
+//
+
+import SwiftUI
+
+@main
+struct LearnAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
