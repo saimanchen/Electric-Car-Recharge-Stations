52a9c083de67280c302c1d97150b3500475760e2
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-21 10:14:37 +0200

    add view:  StationView() - background image

--- LearnAPI/ContentView.swift
+++ LearnAPI/ContentView.swift
@@ -19,8 +19,37 @@ struct ContentView: View {
     }
 }
 
+struct StationView: View {
+    var body: some View {
+        ZStack {
+            // background image
+            AsyncImage(
+                url:
+                    URL(string: "https://resources.news.e.abb.com/attachments/published/82941/en-US/3081E72A5C7C/ABB_launches_Terra_360_usecase_refueling_station_station_render_dpi.jpg"),
+                
+                content: { image in
+                    image.resizable()
+                },
+                
+                placeholder: {
+                    Text("Loading...")
+                        .foregroundColor(.white)
+                        .font(.title)
+                })
+            
+            VStack {
+                
+            }
+        }
+        .frame(width: 350, height: 250)
+        .background(.black)
+        .cornerRadius(10)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //        ContentView()
+        StationView()
     }
 }
