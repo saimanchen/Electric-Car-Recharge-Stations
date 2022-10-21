7fde3cbc033eb4ea835637f1cb6ba5ac0900bc62
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-21 10:34:00 +0200

    update: StationView()
    Gradient, and text alignment

--- LearnAPI/ContentView.swift
+++ LearnAPI/ContentView.swift
@@ -21,29 +21,42 @@ struct ContentView: View {
 
 struct StationView: View {
     var body: some View {
-        ZStack {
+        ZStack(alignment: .leading) {
             // background image
             AsyncImage(
                 url:
                     URL(string: "https://resources.news.e.abb.com/attachments/published/82941/en-US/3081E72A5C7C/ABB_launches_Terra_360_usecase_refueling_station_station_render_dpi.jpg"),
                 
                 content: { image in
-                    image.resizable()
+                    image.resizable().opacity(0.8)
                 },
                 
                 placeholder: {
                     Text("Loading...")
                         .foregroundColor(.white)
                         .font(.title)
-                })
+                }).overlay(
+                    LinearGradient(
+                        gradient: Gradient(
+                            colors: [
+                                Color.black.opacity(0.01),
+                                Color.black]),
+                        startPoint: .center,
+                        endPoint: .bottom))
             
-            VStack {
-                
-            }
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("Title")
+                    .font(.largeTitle)
+                    .bold()
+                Text("... description")
+                    .font(.caption)
+            }.foregroundColor(.white).padding()
         }
         .frame(width: 350, height: 250)
         .background(.black)
         .cornerRadius(10)
+        .padding()
     }
 }
 
